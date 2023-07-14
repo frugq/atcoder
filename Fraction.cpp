e1a9184159ead7b68421781a1dcf35e34a08af31
frugq <104624951+frugq@users.noreply.github.com>
2023-07-14 21:06:53 +0900

    分数
    https://atcoder.jp/contests/abc308/submissions/43558062

---
+++ Fraction.cpp
@@ -0,0 +1,102 @@
+class Fraction{
+public:
+    ll num = 0;
+    ll den = 1;
+
+    Fraction(){};
+    Fraction(ll Num) : num(Num){};
+    Fraction(ll Num, ll Den) : num(Num), den(Den){};
+
+    void reduction();
+    Fraction operator +(Fraction y);
+    Fraction operator -(Fraction y);
+    Fraction operator -();
+    Fraction operator *(Fraction y);
+    Fraction operator /(Fraction y);
+    void operator +=(Fraction y);
+    void operator -=(Fraction y);
+    void operator *=(Fraction y);
+    void operator /=(Fraction y);
+    bool operator ==(Fraction y) const;
+    bool operator !=(Fraction y) const;
+    bool operator <(Fraction y) const;
+    bool operator >(Fraction y) const;
+};
+string to_string(Fraction x);
+Fraction operator *(ll x, Fraction y);
+
+void Fraction::reduction(){
+    ll g = gcd(num, den);
+    num /= g;
+    den /= g;
+    if(den < 0){
+        num *= -1;
+        den *= -1;
+    }
+}
+
+Fraction Fraction::operator +(Fraction y){
+    Fraction result(num * y.den + y.num * den, den * y.den);
+    result.reduction();
+    return result;
+}
+
+Fraction Fraction::operator -(Fraction y){
+    return *this + (-y);
+}
+
+Fraction Fraction::operator -(){
+    return *this * (-1);
+}
+
+Fraction Fraction::operator *(Fraction y){
+    Fraction result(num * y.num, den * y.den);
+    result.reduction();
+    return result;
+}
+
+Fraction operator *(ll x, Fraction y){
+    return Fraction(x) * y;
+}
+
+Fraction Fraction::operator /(Fraction y){
+    Fraction result(num * y.den, den * y.num);
+    result.reduction();
+    return result;
+}
+
+void Fraction::operator +=(Fraction y){
+    *this = *this + y;
+}
+
+void Fraction::operator -=(Fraction y){
+    *this = *this - y;
+}
+
+void Fraction::operator *=(Fraction y){
+    *this = *this * y;
+}
+
+void Fraction::operator /=(Fraction y){
+    *this = *this / y;
+}
+
+bool Fraction::operator ==(Fraction y) const{
+    return num * y.den == den * y.num;
+}
+
+bool Fraction::operator !=(Fraction y) const{
+    return !(*this == y);
+}
+
+bool Fraction::operator <(Fraction y) const{
+    return num * y.den < den * y.num;
+}
+
+bool Fraction::operator >(Fraction y) const{
+    return num * y.den > den * y.num;
+}
+
+string to_string(Fraction x){
+    return std::to_string(x.num) + "/" + std::to_string(x.den);
+}
