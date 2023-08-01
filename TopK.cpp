395b494fb36f0045af0f06971691527c0847456b
frugq <104624951+frugq@users.noreply.github.com>
2023-08-01 10:16:37 +0900

    多重集合のk番目
    https://atcoder.jp/contests/abc306/submissions/43493207
    https://atcoder.jp/contests/abc312/submissions/44149451

--- TopK.cpp
+++ TopK.cpp
@@ -1,7 +1,7 @@
 template<class T, class Compare = less<T>>
 class TopK{
 public:
-    int k;
+    int k;              //1-indexed
     multiset<T, Compare> st_top, st_bottom;
     T sum_stTop = 0;
 
@@ -11,14 +11,15 @@ public:
     void erase(T val);
     T getKthVal();      //小さい方からk番目の数
     T getKthSum();      //小さい方からk番目までの合計
-    int getSize();
+    int size();
+    void changeK(int newK);
 };
 
 template<class T, class Compare>
 TopK<T, Compare>::TopK(int K, vector<T> Vec){
     k = K;
     sort(Vec.begin(), Vec.end(), Compare());
-    for(int i = 0; i < size(Vec); ++i){
+    for(int i = 0; i < SIZE(Vec); ++i){
         if(i < k){
             st_top.emplace(Vec.at(i));
             sum_stTop += Vec.at(i);
@@ -32,7 +33,7 @@ template<class T, class Compare>
 void TopK<T, Compare>::add(T val){
     st_top.emplace(val);
     sum_stTop += val;
-    if(size(st_top) > k){
+    if(SIZE(st_top) > k){
         auto itr = st_top.end();
         itr--;
         st_bottom.emplace(*itr);
@@ -49,7 +50,7 @@ void TopK<T, Compare>::erase(T val){
         assert(itr != st_top.end());
         st_top.erase(itr);
         sum_stTop -= val;
-        if(size(st_bottom) > 0){
+        if(SIZE(st_bottom) > 0){
             itr = st_bottom.begin();
             st_top.emplace(*itr);
             sum_stTop += *itr;
@@ -65,7 +66,7 @@ void TopK<T, Compare>::erase(T val){
 
 template<class T, class Compare>
 T TopK<T, Compare>::getKthVal(){
-    assert(size(st_top) == k);
+    assert(SIZE(st_top) == k);
     auto itr = st_top.end();
     itr--;
     return *itr;
@@ -77,6 +78,24 @@ T TopK<T, Compare>::getKthSum(){
 }
 
 template<class T, class Compare>
-int TopK<T, Compare>::getSize(){
-    return size(st_top) + size(st_bottom);
+int TopK<T, Compare>::size(){
+    return SIZE(st_top) + SIZE(st_bottom);
+}
+
+template<class T, class Compare>
+void TopK<T, Compare>::changeK(int newK){
+    k = newK;
+    while(SIZE(st_top) > k){
+        auto itr = st_top.end();
+        itr--;
+        sum_stTop -= *itr;
+        st_bottom.emplace(*itr);
+        st_top.erase(itr);
+    }
+    while(SIZE(st_top) < k && SIZE(st_bottom) > 0){
+        auto itr = st_bottom.begin();
+        sum_stTop += *itr;
+        st_top.emplace(*itr);
+        st_bottom.erase(itr);
+    }
 }
