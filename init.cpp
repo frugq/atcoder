7ac898351944fc894de3bfa368f7b6c02ecb0e17
frugq <104624951+frugq@users.noreply.github.com>
2024-02-06 22:29:20 +0900

    ユーザースニペット

---
+++ init.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#include <atcoder/all>
+//using namespace atcoder;
+#ifdef __LOCAL
+#include "debug2.h"
+#endif
+#ifndef __LOCAL
+#define show(x) 
+#define SET_GROUP(x)
+#endif
+#define SIZE(x) (int)(x.size())
+#define ALL(x) x.begin(),x.end()
+using ll = long long;
+template<class T>bool chmin(T& x,T y){if(x>y){x=y;return true;}return false;}
+template<class T>bool chmax(T& x,T y){if(x<y){x=y;return true;}return false;}
+template<class T>vector<T>matrix(int n1,T val){return vector<T>(n1,val);}
+template<class T>vector<vector<T>>matrix(int n1,int n2,T val){return vector<vector<T>>(n1,matrix<T>(n2,val));}
+template<class T>vector<vector<vector<T>>>matrix(int n1,int n2,int n3,T val){return vector<vector<vector<T>>>(n1,matrix<T>(n2,n3,val));}
+template<class T>vector<vector<vector<vector<T>>>>matrix(int n1,int n2,int n3,int n4,T val){return vector<vector<vector<vector<T>>>>(n1,matrix<T>(n2,n3,n4,val));}
+
+int main(){
+    
+    return 0;
+}
