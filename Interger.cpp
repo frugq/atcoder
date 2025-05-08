e6d4277a0443ca7181d780e85ec82698fbca5f2f
frugq <104624951+frugq@users.noreply.github.com>
2025-05-09 06:45:55 +0900

    Update Interger.cpp
    整数演算

--- Interger.cpp
+++ Interger.cpp
@@ -1,86 +1,84 @@
 //整数範囲の計算ライブラリ
-template<class T>
 class Interger{
 public:
-    T val;
+    bigint val;
     
     Interger(){}
-
     Interger(ll Val){
-        val = T(Val);
+        val = Val;
     }
-    Interger(T Val){
+    Interger(bigint Val){
         val = Val;
     }
 
-    bool operator<(const Interger<T>& y) const{
+    bool operator<(const Interger& y) const{
         return val < y.val;
     }
     
-    bool operator<=(const Interger<T>& y) const{
+    bool operator<=(const Interger& y) const{
         return val <= y.val;
     }
 
-    bool operator>(const Interger<T>& y) const{
+    bool operator>(const Interger& y) const{
         return val > y.val;
     }
 
-    bool operator>=(const Interger<T>& y) const{
+    bool operator>=(const Interger& y) const{
         return val >= y.val;
     }
 
-    bool operator==(const Interger<T>& y) const{
+    bool operator==(const Interger& y) const{
         return val == y.val;
     }
 
-    bool operator!=(const Interger<T>& y) const{
+    bool operator!=(const Interger& y) const{
         return !(*this == y.val);
     }
     
-    Interger operator +(const Interger<T>& y) const{
+    Interger operator +(const Interger& y) const{
         return Interger(val + y.val);
     }
 
-    Interger operator -(const Interger<T>& y) const{
+    Interger operator -(const Interger& y) const{
         return Interger(val - y.val);
     }
     
-    Interger operator *(const Interger<T>& y) const{
+    Interger operator *(const Interger& y) const{
         return Interger(val * y.val);
     }
 
-    Interger operator /(const Interger<T>& y) const{
+    Interger operator /(const Interger& y) const{
         return Interger(val / y.val);
     }
 
-    void operator +=(const Interger<T>& y) const{
+    void operator +=(const Interger& y){
         val += y.val;
     }
     
-    void operator -=(const Interger<T>& y) const{
+    void operator -=(const Interger& y){
         val -= y.val;
     }
     
-    void operator *=(const Interger<T>& y) const{
+    void operator *=(const Interger& y){
         val *= y.val;
     }
     
-    void operator /=(const Interger<T>& y) const{
+    void operator /=(const Interger& y){
         val /= y.val;
     }
 
-    Interger pow(const Interger<T>& y) const{
-        T result = 1;
+    Interger pow(const Interger& y) const{
+        bigint result = 1;
         rep(i, y.val){
             result *= val;
         }
         return result;
     }
 
-    Interger sqrt(const Interger<T>& n = 2, const Interger<T>& maxAns = Interger<T>(T(1 << 30))) const{//floor(√x) 範囲注意
-        Interger<T> l = T(-1), r = maxAns;
-        while(r - l > Interger<T>(1)){
-            Interger<T> m = (l + r) / Interger<T>(2);
+    Interger sqrt(const Interger& n = 2) const{//floor(√x) 範囲注意
+        Interger l = bigint(-1), r = bigint(2 * std::pow((double)(val), 1.0 / (double)(n.val)) + 10);
+        while(r - l > Interger(1)){
+            Interger m = (l + r) / Interger(2);
             if(m.pow(n) <= *this){
                 l = m;
             }else{
@@ -90,11 +88,10 @@ public:
         return l;
     }
 
-    Interger log(const Interger<T>& base, const Interger<T>& maxAns = Interger<T>(T(60))) const{//log_base(x)
-        Interger<T> l = T(-1), r = maxAns;
-        while(r - l > Interger<T>(1)){
-            Interger<T> m = (l + r) / Interger<T>(2);
-            show(m);
+    Interger log(const Interger& base) const{//log_base(x)
+        Interger l = bigint(-1), r = bigint(2 * std::log((double)(base.val)) /  std::log((double)(val)) + 10);
+        while(r - l > 1){
+            Interger m = (l + r) / 2;
             if(base.pow(m) <= *this){
                 l = m;
             }else{
@@ -105,7 +102,7 @@ public:
     }
 
     #ifdef __LOCAL
-    friend int push(const Interger<T>& y){
+    friend int push(const Interger& y){
         debug::push(y.val);
         return 1;
     }
